11c9b3dba946941cba2ad29fca13b4e6315fcc9d
TahaErken <erken.taha@gmail.com>
2022-12-03 13:07:46 +0300

    udp-nmea parser

--- src/main.java
+++ src/main.java
@@ -1,4 +1,5 @@
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class main{
     static String GPGGA_Test = "$GPGGA,211034,4738.9577,N,12220.9329,W,1,09,1.0,10.8,M,-18.4,M,,*42";
@@ -7,9 +8,25 @@ public class main{
 
     public static void main(String [ ] args)
     {
+        int port = 50001;
+        UdpUnicastClient client = new UdpUnicastClient(port);
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(client);
+
+
+
+
+
+
         NMEA nmea = new NMEA();
         nmea.parse(GPGGA_Test);
         System.out.println(nmea.position);
+
+
+
+
+
+
 
     }
 }
